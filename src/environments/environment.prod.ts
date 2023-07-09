8116ef159552c5863780fd8d94c7cc9d7616fd92
Edy <edyxicon@gmail.com>
2023-07-09 13:35:35 -0600

    Add env ready

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,3 @@
-// export const environment = {
-//   production: false,
-//   base_url: 'http://localhost:3000/api',
-// };
-
 export const environment = {
   production: true,
   base_url: 'https://admin-pro-backend-b0d1.onrender.com/api',
